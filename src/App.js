11ce20f77b5bb27219dcc81d9e32e4e275ac5379
Darren Ing <darren.prozz@gmail.com>
2024-02-28 23:30:16 -0500

    Finished weight calculations

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import './App.css';
 function App() {
   const handleCalculate = (event) => {
     event.preventDefault();
-    plates = calculate(weight);
+    setPlates(calculate(weight));
+    console.log(platesObj);
   }
+  const platesObj = {
+    plate55: 0,
+    plate45: 0,
+    plate35: 0,
+    plate25: 0,
+    plate10: 0,
+    plate5: 0,
+    plate2p5: 0,
+    plate1: 0,
+  };
   const [weight, setWeight] = useState("");
-  const [plates, getPlates] = useState("");
+  const [plates, setPlates] = useState("");
   return (
     <div className="App">
       <header className="App-header">
@@ -39,46 +50,37 @@ function App() {
     </div>
 
   );
-}
-
-function calculate(weight) {
-  let w = Number(weight);
-  let plate45 = 0;
-  let plate35 = 0;
-  let plate25 = 0;
-  let plate10 = 0;
-  let plate5 = 0;
-  let plate2p5 = 0;
-  let plate1 = 0;
-
-  while (w > 0){
-    if (w>=90){
-      plate45 += 1;
-      w -= 90;
-    } else if (w>=70){
-      plate35 += 1;
-      w -= 70;
-    } else if (w>=50){
-      plate25 += 1;
-      w -= 50;
-    } else if (w>=20){
-      plate10 += 1;
-      w -= 20;
-    } else if (w>=10){
-      plate5 += 1;
-      w -= 10;
-    } else if (w>=5){
-      plate2p5 += 1;
-      w -= 5;
-    } else if (w>=2){
-      plate1 += 1;
-      w -= 2;
-    } else break;
-
+  function calculate(weight) {
+    let w = Number(weight-45);
+  
+    while (w > 0){
+      if (w >=110){
+        platesObj.plate55 +=1;
+        w -=110;
+      }else if (w>=90){
+        platesObj.plate45 += 1;
+        w -= 90;
+      } else if (w>=70){
+        platesObj.plate35 += 1;
+        w -= 70;
+      } else if (w>=50){
+        platesObj.plate25 += 1;
+        w -= 50;
+      } else if (w>=20){
+        platesObj.plate10 += 1;
+        w -= 20;
+      } else if (w>=10){
+        platesObj.plate5 += 1;
+        w -= 10;
+      } else if (w>=5){
+        platesObj.plate2p5 += 1;
+        w -= 5;
+      } else break;
+  
+    }
+    return platesObj;
+  
   }
-
-  return [plate45,plate35,plate25,plate10,plate5,plate2p5,plate1];
-
 }
 
 export default App;
